61a022df845145a70ed29405ed94a0400ab7a6bb
Dmitriy Golovach <dmitry.glvch@gmail.com>
2022-02-25 11:51:28 +0300

    daemon routines cleanup

--- app/daemon/include/routines.hpp
+++ app/daemon/include/routines.hpp
@@ -14,31 +14,5 @@ listen(
 boost::asio::awaitable<void>
 service (boost::asio::ip::tcp::socket&& socket);
 
-boost::asio::awaitable<std::string>
-receive_string (boost::asio::ip::tcp::socket& socket);
-
-boost::asio::awaitable<void>
-send_go_on (boost::asio::ip::tcp::socket& socket, uint8_t go_on = 1);
-
-boost::asio::awaitable<uint64_t>
-receive_length (boost::asio::ip::tcp::socket& socket);
-
-template <typename T>
-concept bufferable = requires (T obj) {
-  boost::asio::buffer(obj, std::declval<std::size_t>());
-};
-
-boost::asio::awaitable<void>
-receive_bytes (
-    boost::asio::ip::tcp::socket& from,
-    bufferable auto& to,
-    std::size_t byte_count);
-
-boost::asio::awaitable<void>
-send_bytes (
-    boost::asio::ip::tcp::socket& to,
-    bufferable auto& from,
-    std::size_t byte_count);
-
 }
 
